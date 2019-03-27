4a1085b044271556797b6afda59083508e29fb7d
github username <github email address>
2019-03-27 12:52:16 -0500

    sign up and log out works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,5 +22,5 @@ class SessionsController < ApplicationController
    session[:user_id] = nil
    redirect_to new_session_path
  end
- 
+  
 end   
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require_relative '../config/environment'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Recipe.destroy_all
 User.destroy_all
  
 adam = User.new({ :username => "Adam Moran",  :password => "Adam", :translator => false })
