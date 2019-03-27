dcbf7d23590f1f26a1939820ce5bbba58af57200
github username <github email address>
2019-03-27 10:17:04 -0500

    login works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    # before_action :authenticate
-    # def authenticate
-    #     if session[:current_user_id] == nil || User.find(session[:current_user_id]) == nil  # this is the senario where a user hasn't logged in. This is not necessary after sessions.
-    #       redirect_to '/login'
-    #     end
-    # end
-end
   
+  private
+    def current_user
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]   # ||= so current user is only requested one time per request
+    end
+    helper_method :current_user  #set up for @current_user to be accessible in views
+
+    #def authenticate
+     #   redirect_to new_session_path, aler:'Not authorized' if current_user.nil?
+    #end
+end
+   
