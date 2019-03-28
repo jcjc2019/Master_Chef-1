dff88d3752ae90d54a5d42716d6467418892af68
github username <github email address>
2019-03-28 08:40:19 -0500

    user cook_book association complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   patch '/recipes/:id/add_likes', to: 'recipes#add_likes' #add patch route under this route. 
   post '/sessions/new', to: 'sessions#create' 
   delete '/logout', to: 'sessions#destroy'
-  #post '/cook_books/new?', to: 'cook_books#create'
+  #post '/cook_books/:id', to: 'cook_books#show'
   #delete '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
